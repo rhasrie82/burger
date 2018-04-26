31b0a320e87357b0ca98661a2dacfc1b6c550ff1
rhasrie82 <33564425+rhasrie82@users.noreply.github.com>
2018-04-25 22:12:48 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Whopper', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Big Mac', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('McChicken', false, CURRENT_TIMESTAMP);
